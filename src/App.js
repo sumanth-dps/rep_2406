a5f6b3e3fe7a26dd94f89d26d3e3c397a815eb6f
Sumanth <dondamuripavansatyasumanth@gmail.com>
2024-10-16 15:51:53 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bhirava</h1>
+      <img src="./bhirava/bhirava.jpg"></img>
+    </div>
+  );
+}
+
+export default App;
